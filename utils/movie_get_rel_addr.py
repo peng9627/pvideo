dd61e847752dbabb8e1900dc51264c555b3321b8
jim <pengyi9627@gmail.com>
2021-04-03 19:58:25 +0800

    del chrome

--- utils/movie_get_rel_addr.py
+++ utils/movie_get_rel_addr.py
@@ -1,14 +1,10 @@
 import base64
-import json
-import os
 import time
 import traceback
 from urllib import parse
 
 import requests
 from pycore.data.database import mysql_connection
-from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
 
 from data.database import data_vip_video_url
 
@@ -196,54 +192,54 @@ def getadds(addr, not_line):
                 url = getadd16(addr)
             elif p.name == '17':
                 url = getadd17(addr)
-            else:
-                chrome_options = webdriver.ChromeOptions()
-                chrome_options.add_argument('--headless')  # 无界面
-                chrome_options.add_argument("--incognito")
-                chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在报错问题
-                chrome_options.add_argument('--disable-gpu')  # 禁用GPU硬件加速。如果软件渲染器没有就位，则GPU进程将不会启动。
-                chrome_options.add_argument('--disable-dev-shm-usage')
-                chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
-                chrome_options.add_argument(
-                    'User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36')
-                prefs = {"profile.managed_default_content_settings.images": 2}
-                chrome_options.add_experimental_option("prefs", prefs)
-                # 核心代码
-                chrome_options.add_experimental_option('w3c', False)
-                caps = DesiredCapabilities.CHROME
-                caps['loggingPrefs'] = {'performance': 'ALL'}
-                driver = None
-                try:
-                    # 核心代码结束
-                    # driver = webdriver.Chrome('D:/software/phantomjs/bin/chromedriver.exe', desired_capabilities=caps,
-                    #                           options=chrome_options)
-                    driver = webdriver.Chrome(desired_capabilities=caps, options=chrome_options)
-                    driver.set_page_load_timeout(8)
-                    driver.set_script_timeout(8)
-                    driver.get(p.address + addr)
-                    t = 20
-                    find = False
-                    while t > 0:
-                        time.sleep(0.3)
-                        log = driver.get_log('performance')
-                        for li in (s for s in log if ('.m3u8' in s['message'] or '.mp4' in s['message'])):
-                            message_obj = json.loads(li['message'])
-                            if 'request' in message_obj['message']['params']:
-                                url = message_obj['message']['params']['request']['url']
-                                if url.endswith(".m3u8") or url.endswith(".mp4") or '.m3u8?' in url or '.mp4?' in url:
-                                    find = True
-                                    break
-                        if find:
-                            break
-                        t -= 1
-                except:
-                    print(traceback.format_exc())
-                finally:
-                    if driver is not None:
-                        driver.quit()
-                    # os.system('kill -s 9 `pgrep chrome`')
-                    # os.system('taskkill /im chromedriver.exe /F')
-                    # os.system('taskkill /im chrome.exe /F')
+            # else:
+            #     chrome_options = webdriver.ChromeOptions()
+            #     chrome_options.add_argument('--headless')  # 无界面
+            #     chrome_options.add_argument("--incognito")
+            #     chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在报错问题
+            #     chrome_options.add_argument('--disable-gpu')  # 禁用GPU硬件加速。如果软件渲染器没有就位，则GPU进程将不会启动。
+            #     chrome_options.add_argument('--disable-dev-shm-usage')
+            #     chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+            #     chrome_options.add_argument(
+            #         'User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36')
+            #     prefs = {"profile.managed_default_content_settings.images": 2}
+            #     chrome_options.add_experimental_option("prefs", prefs)
+            #     # 核心代码
+            #     chrome_options.add_experimental_option('w3c', False)
+            #     caps = DesiredCapabilities.CHROME
+            #     caps['loggingPrefs'] = {'performance': 'ALL'}
+            #     driver = None
+            #     try:
+            #         # 核心代码结束
+            #         # driver = webdriver.Chrome('D:/software/phantomjs/bin/chromedriver.exe', desired_capabilities=caps,
+            #         #                           options=chrome_options)
+            #         driver = webdriver.Chrome(desired_capabilities=caps, options=chrome_options)
+            #         driver.set_page_load_timeout(8)
+            #         driver.set_script_timeout(8)
+            #         driver.get(p.address + addr)
+            #         t = 20
+            #         find = False
+            #         while t > 0:
+            #             time.sleep(0.3)
+            #             log = driver.get_log('performance')
+            #             for li in (s for s in log if ('.m3u8' in s['message'] or '.mp4' in s['message'])):
+            #                 message_obj = json.loads(li['message'])
+            #                 if 'request' in message_obj['message']['params']:
+            #                     url = message_obj['message']['params']['request']['url']
+            #                     if url.endswith(".m3u8") or url.endswith(".mp4") or '.m3u8?' in url or '.mp4?' in url:
+            #                         find = True
+            #                         break
+            #             if find:
+            #                 break
+            #             t -= 1
+            #     except:
+            #         print(traceback.format_exc())
+            #     finally:
+            #         if driver is not None:
+            #             driver.quit()
+            #         # os.system('kill -s 9 `pgrep chrome`')
+            #         # os.system('taskkill /im chromedriver.exe /F')
+            #         # os.system('taskkill /im chrome.exe /F')
             if len(url) > 0 and (url.endswith(".m3u8") or url.endswith(".mp4") or '.m3u8?' in url or '.mp4?' in url):
                 name = p.name
                 break
@@ -283,52 +279,52 @@ def check_adds(addr):
                 url = getadd16(addr)
             elif p.name == '17':
                 url = getadd17(addr)
-            else:
-                chrome_options = webdriver.ChromeOptions()
-                chrome_options.add_argument('--headless')  # 无界面
-                chrome_options.add_argument("--incognito")
-                chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在报错问题
-                chrome_options.add_argument('--disable-gpu')  # 禁用GPU硬件加速。如果软件渲染器没有就位，则GPU进程将不会启动。
-                chrome_options.add_argument('--disable-dev-shm-usage')
-                chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
-                chrome_options.add_argument(
-                    'User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36')
-                prefs = {"profile.managed_default_content_settings.images": 2}
-                chrome_options.add_experimental_option("prefs", prefs)
-                # 核心代码
-                chrome_options.add_experimental_option('w3c', False)
-                caps = DesiredCapabilities.CHROME
-                caps['loggingPrefs'] = {'performance': 'ALL'}
-                # 核心代码结束
-                # driver = webdriver.Chrome('D:/software/phantomjs/bin/chromedriver.exe', desired_capabilities=caps,
-                #                           options=chrome_options)
-                driver = webdriver.Chrome(desired_capabilities=caps, options=chrome_options)
-                try:
-                    driver.set_page_load_timeout(8)
-                    driver.set_script_timeout(8)
-                    driver.get(p.address + addr)
-                    t = 20
-                    find = False
-                    while t > 0:
-                        time.sleep(0.3)
-                        log = driver.get_log('performance')
-                        for li in (s for s in log if ('.m3u8' in s['message'] or '.mp4' in s['message'])):
-                            message_obj = json.loads(li['message'])
-                            if 'request' in message_obj['message']['params']:
-                                url = message_obj['message']['params']['request']['url']
-                                if url.endswith(".m3u8") or url.endswith(".mp4") or '.m3u8?' in url or '.mp4?' in url:
-                                    find = True
-                                    break
-                        if find:
-                            break
-                        t -= 1
-                except:
-                    print(traceback.format_exc())
-                finally:
-                    driver.quit()
-                    # os.system('kill -s 9 `pgrep chrome`')
-                    # os.system('taskkill /im chromedriver.exe /F')
-                    # os.system('taskkill /im chrome.exe /F')
+            # else:
+            #     chrome_options = webdriver.ChromeOptions()
+            #     chrome_options.add_argument('--headless')  # 无界面
+            #     chrome_options.add_argument("--incognito")
+            #     chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在报错问题
+            #     chrome_options.add_argument('--disable-gpu')  # 禁用GPU硬件加速。如果软件渲染器没有就位，则GPU进程将不会启动。
+            #     chrome_options.add_argument('--disable-dev-shm-usage')
+            #     chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+            #     chrome_options.add_argument(
+            #         'User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36')
+            #     prefs = {"profile.managed_default_content_settings.images": 2}
+            #     chrome_options.add_experimental_option("prefs", prefs)
+            #     # 核心代码
+            #     chrome_options.add_experimental_option('w3c', False)
+            #     caps = DesiredCapabilities.CHROME
+            #     caps['loggingPrefs'] = {'performance': 'ALL'}
+            #     # 核心代码结束
+            #     # driver = webdriver.Chrome('D:/software/phantomjs/bin/chromedriver.exe', desired_capabilities=caps,
+            #     #                           options=chrome_options)
+            #     driver = webdriver.Chrome(desired_capabilities=caps, options=chrome_options)
+            #     try:
+            #         driver.set_page_load_timeout(8)
+            #         driver.set_script_timeout(8)
+            #         driver.get(p.address + addr)
+            #         t = 20
+            #         find = False
+            #         while t > 0:
+            #             time.sleep(0.3)
+            #             log = driver.get_log('performance')
+            #             for li in (s for s in log if ('.m3u8' in s['message'] or '.mp4' in s['message'])):
+            #                 message_obj = json.loads(li['message'])
+            #                 if 'request' in message_obj['message']['params']:
+            #                     url = message_obj['message']['params']['request']['url']
+            #                     if url.endswith(".m3u8") or url.endswith(".mp4") or '.m3u8?' in url or '.mp4?' in url:
+            #                         find = True
+            #                         break
+            #             if find:
+            #                 break
+            #             t -= 1
+            #     except:
+            #         print(traceback.format_exc())
+            #     finally:
+            #         driver.quit()
+            #         # os.system('kill -s 9 `pgrep chrome`')
+            #         # os.system('taskkill /im chromedriver.exe /F')
+            #         # os.system('taskkill /im chrome.exe /F')
             if len(url) > 0 and (url.endswith(".m3u8") or url.endswith(".mp4") or '.m3u8?' in url or '.mp4?' in url):
                 data_vip_video_url.update_ping(connection, p.id, int(time.time() * 1000 - times))
             else:
